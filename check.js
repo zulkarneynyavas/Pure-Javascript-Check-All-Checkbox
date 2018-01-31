9b64d9e980c2b3d3ba52275d4461293d364ac4fd
Zülkarneyn Yavaş <zulkarneynyavas@gmail.com>
2018-01-31 20:17:40 +0300

    Create check.js

---
+++ check.js
@@ -0,0 +1,14 @@
+function SelectAll() {
+  var a = document.form,
+    b = a.elements['CheckBox'],
+    c = b.length,
+    d,
+    e = a.elements['CheckAll'];
+  for (d=0; d<c; d++) {
+    if (e.checked == true) {
+      b[d].checked = true;
+    } else {
+      b[d].checked = false;
+    }
+  }
+}
